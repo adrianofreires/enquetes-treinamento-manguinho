b7357838eefe620807d5c71157a00e46339b7628
Adriano Freires <freires.adriano@gmail.com>
2021-09-30 15:32:18 -0300

    Aula 4.3

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,37 +1,52 @@
 import 'package:flutter/material.dart';
 
+import '../components/components.dart';
+
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              child: Image(image: AssetImage('lib/ui/assets/logo.png')),
+            LoginHeader(),
+            HeadLine1(
+              text: 'Login',
             ),
-            Text('Login'.toUpperCase()),
-            Form(
-                child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: 'E-mail', icon: Icon(Icons.email)),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: 'Senha', icon: Icon(Icons.lock)),
-                  obscureText: true,
-                ),
-                ElevatedButton(
-                    onPressed: () {}, child: Text('Entrar'.toUpperCase())),
-                TextButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.person),
-                    label: Text('Criar Conta')),
-              ],
-            )),
+            Padding(
+              padding: EdgeInsets.all(32.0),
+              child: Form(
+                  child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'E-mail',
+                        icon: Icon(
+                          Icons.email,
+                          color: Theme.of(context).primaryColorLight,
+                        )),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 8.0, bottom: 32),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          labelText: 'Senha',
+                          icon: Icon(Icons.lock,
+                              color: Theme.of(context).primaryColorLight)),
+                      obscureText: true,
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {}, child: Text('Entrar'.toUpperCase(),)),
+                  TextButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.person),
+                      label: Text('Criar Conta')),
+                ],
+              )),
+            ),
           ],
         ),
       ),
